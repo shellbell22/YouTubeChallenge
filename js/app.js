d1bc174eaad583d64b2411074910b1cd0ad405a8
shellbell22 <morris.shelley@gmail.com>
2016-07-31 11:35:35 -0400

    initial commit

---
+++ js/app.js
@@ -0,0 +1,28 @@
+$(function() {
+    $('#search-term').submit(function(event) {
+        event.preventDefault();
+        var searchTerm = $('#query').val();
+        getRequest(searchTerm);
+    });
+});
+
+function getRequest(searchTerm) {
+	var params = {
+		s: searchTerm,
+		r: 'json'
+	};
+	url = 'http://www.omdbapi.com';
+
+	$.getJSON(url, params, function(data) {
+		showResults(data.Search);
+	});
+}
+
+function showResults(results) {
+    var html = "";
+    $.each(results, function(index, value) {
+        html += '<p>' + value.Title + '<p>';
+        console.log(value.Title);
+    });
+    $('#search-results').html(html);
+}
