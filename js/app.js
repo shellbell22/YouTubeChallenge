720083abc1bf1de0557bfbb001b32264dd3c3401
shellbell22 <morris.shelley@gmail.com>
2016-07-31 19:08:05 -0400

    final commit

--- js/app.js
+++ js/app.js
@@ -1,28 +1,49 @@
+var token;
+
 $(function() {
     $('#search-term').submit(function(event) {
         event.preventDefault();
         var searchTerm = $('#query').val();
+        $('#search-results').empty();
         getRequest(searchTerm);
+        
     });
+
+    $('#nextpage').click(function () {
+        var searchTerm = $('#query').val();
+
+        getRequest(searchTerm, token);
+
+    });
+
 });
 
-function getRequest(searchTerm) {
+function getRequest(searchTerm, tokenInput) {
 	var params = {
-		s: searchTerm,
-		r: 'json'
+		part:'snippet',
+    key: 'AIzaSyA3_pbYGu7QwIGraJ7MwH0eM09KZR9U2yA',
+    q: searchTerm,
+    pageToken: tokenInput
 	};
-	url = 'http://www.omdbapi.com';
+	url = 'https://www.googleapis.com/youtube/v3/search';
 
 	$.getJSON(url, params, function(data) {
-		showResults(data.Search);
+      console.log(data);
+      token = data.nextPageToken;
+		showResults(data.items);
 	});
 }
+
 
 function showResults(results) {
     var html = "";
     $.each(results, function(index, value) {
-        html += '<p>' + value.Title + '<p>';
-        console.log(value.Title);
+        var thumbnailimage = value.snippet.thumbnails.medium.url;
+        var largethumbnailimage = value.snippet.thumbnails.high.url;
+        var channellink = 'https://www.youtube.com/channel/' + value.snippet.channelId;
+        var channelname = value.snippet.channelTitle;
+        var link = 'https://www.youtube.com/watch?v=' +  value.id.videoId;
+        html += '<li><a target="_new" href="'+link+'"><h1>'+ value.snippet.title +'</h1></a><a target="_new" href="' + channellink + '"><h2>' + channelname + '</h2></a><a  data-lightbox="' + largethumbnailimage + '" href="' + largethumbnailimage + '"><img src="' + thumbnailimage + '"></a></li>';
     });
-    $('#search-results').html(html);
+    $('#search-results').append(html);
 }
